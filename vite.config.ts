9742fb664d67637bbaa66381509f1604904697d1
webdiller <eugenefromrus@gmail.com>
2024-11-11 14:58:47 +1000

    upd: upd config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from "path"
-
-const pages = {
-  index: resolve(__dirname, './src/pages/index/index.html'),
-  // about: resolve(__dirname, './src/about.html'),
-}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       ...pages
-  //     },
-  //   }
-  // },
   server: {
     port: 3000
   },
